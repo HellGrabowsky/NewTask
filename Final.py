cf040b1b981be551103e518f3574a2900f303b73
Grabowsky <grabowsky91@bk.ru>
2023-12-29 00:30:52 +0600

    Добавлена ссылка на блоко схему

--- Final.py
+++ Final.py
@@ -7,12 +7,4 @@ for s in new_array:
 
 print("Результат:", result_array)
 
-
-# Нарисовать блок-схему алгоритма 
-# Сделать еще 2 ветки
-
-# 7.	git branch - Просмотр списка веток.
-# 8.	git checkout <ветка> - Переключение на другую ветку.
-# 3.	git checkout -b <новая_ветка> - Создание и переключение на новую ветку.
-# 4.	git merge <ветка> - Слияние указанной ветки с текущей.
-
+# https://drive.google.com/file/d/11X_ytNPj1jOFXjJb0TaPLNCY95tpssFj/view?usp=sharing
